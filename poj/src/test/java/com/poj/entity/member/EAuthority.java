da130a3aa42fdcad761e6abe0fce0e277ebaacd2
Hosung <an3735297@naver.com>
2023-10-06 13:50:32 +0900

    feat : entity / Member, Authority
    유저 정보를 저장할 Member entity 와 인증 인가에 사용될 Authority entity 를 만들었습니다.

---
+++ poj/src/test/java/com/poj/entity/member/EAuthority.java
@@ -0,0 +1,5 @@
+package com.poj.entity.member;
+
+public enum EAuthority {
+    ROLE_ADMIN, ROLE_USER, ROLE_UNVERIFIED
+}
---
+++ poj/src/test/java/com/poj/entity/member/Member.java
@@ -0,0 +1,39 @@
+package com.poj.entity.member;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true)
+    private String username;
+
+    @Email
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(  name = "member_authorities",
+            joinColumns = @JoinColumn(name = "member_id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id"))
+    private Set<Authority> authorities = new HashSet<>();
+}
