4c3b75a3c5ad4ab0c8497528778e0b49ee202a77
Hosung <an3735297@naver.com>
2023-10-06 18:25:10 +0900

    fix : repository / querydsl repo 상속

--- poj/src/main/java/com/poj/repository/MemberRepository.java
+++ poj/src/main/java/com/poj/repository/MemberRepository.java
@@ -3,7 +3,7 @@ package com.poj.repository;
 import com.poj.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryQuerydsl {
     Boolean existsByUsername(String username);
     boolean existsByEmail(String email);
 }
