97d5aa40b84dd878e2b6f6e18bc771dfb840eca1
Hosung <an3735297@pusan.ac.kr>
2023-10-09 16:40:07 +0900

    feat : admin / updateToUser
    unverified 한 member 를 user 권한으로 업데이트 하는 서비스를 구현하였습니다.

---
+++ poj/src/main/java/com/poj/service/admin/AdminService.java
@@ -0,0 +1,25 @@
+package com.poj.service.admin;
+
+import com.poj.entity.member.EAuthority;
+import com.poj.entity.member.Member;
+import com.poj.repository.AuthorityRepository;
+import com.poj.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AdminService {
+    private final MemberRepository memberRepository;
+    private final AuthorityRepository authorityRepository;
+
+    @Transactional
+    public void upgradeToUser(Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(
+                () -> new IllegalArgumentException("해당 회원이 존재하지 않습니다.")
+        );
+        member.getAuthorities().add(authorityRepository.findByName(EAuthority.ROLE_USER));
+    }
+}
