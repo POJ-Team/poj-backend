c6d5704feb77333aefedca0558c8006150d9eeed
haenghaeng <haeng@pusan.ac.kr>
2023-10-09 22:37:06 +0900

    Comment : Problem, ProblemRepository
    코드의 상단에 설명하는 주석을 추가하였습니다.
    또한, 아직 구현되지 않은 기능에도 주석을 추가하였습니다.

--- poj/src/main/java/com/poj/entity/problem/Problem.java
+++ poj/src/main/java/com/poj/entity/problem/Problem.java
@@ -9,6 +9,11 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 사용자에게 제공되는 문제.
+ * 번호, 제목, 설명, input/output 예시, 제출한 사람 수, 통과한 사람 수, 통과 비율
+ * 사용자가 문제를 제출할 때마다 addSumitNum을 호출. 만약 맞으면 addPassNum을 이후 호출
+ */
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -21,7 +26,7 @@ public class Problem extends BaseEntity {
 
     @NotBlank
     @Column(unique = true)
-    private Long index; // 번호
+    private Long index; // 번호. 새로운 문제가 추가될 때마다 번호가 증가해야 하는데 아직 그 기능이 없음.
 
     @NotBlank
     private String title; // 제목
