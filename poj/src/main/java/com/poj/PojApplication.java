f8dbaa2930af9e949ea32c317e24a72082922b3f
pnuth <rkdgusxor121@naver.com>
2023-10-11 23:06:50 +0900

    Board 클래스와 EBoard enum 생성(Board는 나중에 추상 클래스로 수정 예정), 제목, 작성자, 글 번호, 내용, 조회수 추가

--- poj/src/main/java/com/poj/PojApplication.java
+++ poj/src/main/java/com/poj/PojApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PojApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PojApplication.class, args);
 	}
---
+++ poj/src/main/java/com/poj/entity/Board/Board.java
@@ -0,0 +1,4 @@
+package com.poj.entity.question;
+public class Question {
+
+}
