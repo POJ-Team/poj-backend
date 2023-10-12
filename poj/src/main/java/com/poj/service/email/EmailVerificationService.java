7bcc9fb8534ba00c01d7f5860aaca132570ce96c
Hosung <an3735297@pusan.ac.kr>
2023-10-12 15:32:54 +0900

    feat : 전송한 인증코드를 검증하는 메서드 구현

--- poj/src/main/java/com/poj/service/email/EmailVerificationService.java
+++ poj/src/main/java/com/poj/service/email/EmailVerificationService.java
@@ -1,7 +1,9 @@
 package com.poj.service.email;
 
+import com.poj.repository.redis.RedisRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
@@ -11,12 +13,23 @@ import java.security.SecureRandom;
 @RequiredArgsConstructor
 public class EmailVerificationService {
     private final EmailSendService emailSendService;
+    private final RedisRepository redisRepository;
+    @Value("${spring.mail.auth-code-validity-in-seconds}")
+    private Long authCodeValidityInSeconds;
     private static final SecureRandom secureRandom = new SecureRandom();
     private static final String TITLE = "POJ 회원가입 인증 코드";
 
     public void sendCodeToEmail(String toEmail) {
         String code = createCode();
         emailSendService.sendEmail(toEmail, TITLE, code);
+        String authCodeKey = getAuthCodeKey(toEmail);
+        redisRepository.setWithTimeout(authCodeKey, code, authCodeValidityInSeconds);
+    }
+
+    public boolean validateCode(String toEmail, String code) {
+        String authCodeKey = getAuthCodeKey(toEmail);
+        String authCode = redisRepository.get(authCodeKey);
+        return authCode != null && authCode.equals(code);
     }
 
     private String createCode() {
@@ -26,5 +39,9 @@ public class EmailVerificationService {
             code.append(random);
         }
         return code.toString();
+    }
+
+    private String getAuthCodeKey(String toEmail) {
+        return "auth_code:" + toEmail;
     }
 }
