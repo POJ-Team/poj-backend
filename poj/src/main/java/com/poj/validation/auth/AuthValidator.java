d08ef6b3681a9a834525d450d13331ded5e5ffbb
Hosung <an3735297@naver.com>
2023-10-13 12:28:12 +0900

    feat : 회원가입 로직에 token 을 받아 검증하는 절차 추가
    회원가입을 요청할 경우 token 을 받아서 검증하는 절차를 추가하였습니다. 하나의 jwt 로 여러개의 회원가입을 진행하는 해킹을 jwt 에 포함된 이메일에 대해서만 회원가입이 진행되도록 함으로써 방지하였습니다.

--- poj/src/main/java/com/poj/validation/auth/AuthValidator.java
+++ poj/src/main/java/com/poj/validation/auth/AuthValidator.java
@@ -2,6 +2,7 @@ package com.poj.validation.auth;
 
 import com.poj.dto.auth.SignupRequest;
 import com.poj.repository.member.MemberRepository;
+import com.poj.security.jwt.JwtValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +10,9 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class AuthValidator {
     private final MemberRepository memberRepository;
+    private final JwtValidator jwtValidator;
     public void signupValidate(SignupRequest signupRequest) {
+        jwtValidator.validateEmailVerificationToken(signupRequest.getToken(), signupRequest.getEmail());
         if (memberRepository.existsByUsername(signupRequest.getUsername())) {
             throw new IllegalArgumentException("이름이 이미 존재합니다.");
         }
