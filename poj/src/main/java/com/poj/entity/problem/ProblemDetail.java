a54049881639adeb157cb8734247953e6bb432df
haenghaeng <haeng@pusan.ac.kr>
2023-10-13 23:15:53 +0900

    fix:ProblemDetail
    주석을 추가하고 JoinColumn의 잘못된 부분을 수정하였습니다. 또한 Limit에 적혀있던 상수를 제거하고 생성자에 넣었습니다.

--- poj/src/main/java/com/poj/entity/problem/ProblemDetail.java
+++ poj/src/main/java/com/poj/entity/problem/ProblemDetail.java
@@ -7,6 +7,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 문제에 대한 상세 정보.
+ */
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -17,7 +21,8 @@ public class ProblemDetail {
     @Column(name = "problem_detail_id")
     private Long id; // ID
 
-    @OneToOne(mappedBy = "problemDetail")
+    @OneToOne
+    @JoinColumn(name = "problem_id")
     private Problem problem;
 
     @NotBlank
@@ -27,8 +32,21 @@ public class ProblemDetail {
     private String inputExample; // input 예시
     private String outputExample; // output 예시
 
-    private Long timeLimit = Long.MAX_VALUE; // 시간 제한(default = Long.MAX_VALUE)
-    private Long memoryLimit = 256L; // 메모리 제한. 단위 = mb(default = 256mb)
+    private Long timeLimit; // 시간 제한(default = Long.MAX_VALUE)
+    private Long memoryLimit; // 메모리 제한. 단위 = mb(default = 256mb)
+
+    protected void setProblem(Problem problem){
+        this.problem = problem;
+    }
+
+    @Builder
+    public ProblemDetail(String info, String inputExample, String outputExample) {
+        this.info = info;
+        this.inputExample = inputExample;
+        this.outputExample = outputExample;
+        this.timeLimit = Long.MAX_VALUE;
+        this.memoryLimit = 256L;;
+    }
 
     @Builder
     public ProblemDetail(String info, String inputExample, String outputExample, Long timeLimit, Long memoryLimit) {
