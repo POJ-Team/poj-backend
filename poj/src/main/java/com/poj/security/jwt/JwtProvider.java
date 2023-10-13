34dd501968c6b1b9bdd167963101d290f2db0d89
Hosung <an3735297@naver.com>
2023-10-13 10:33:18 +0900

    docs : JwtProvider 객체의 문서를 수정하였습니다.
    JwtService 는 현재 프로젝트에 없기 때문에 삭제하였고, 이  객체를 사용하는 서비스는 AtRtService 등이 있습니다. 따라서 문서를 수정하였습니다.

--- poj/src/main/java/com/poj/security/jwt/JwtProvider.java
+++ poj/src/main/java/com/poj/security/jwt/JwtProvider.java
@@ -15,7 +15,7 @@ import java.security.Key;
 import java.util.Date;
 import java.util.stream.Collectors;
 /**
- * 이 객체는 말  그대로 JWT 를 생성"만" 해줍니다. JWT 관련 서비스는 JWTService 에서 제공합니다.<br>
+ * 이 객체는 JWT 를 생성"만" 해줍니다. 관련 서비스는 서비스 객체를 참조해주세요. <br>
  */
 //JwtProvider 는 secret key 가지고, 이를 필요로 하는 다양한 곳에서 활용됩니다.
 //따라서 순환 의존성을 가지지 않도록 JwtProvider 에는 추가적인 의존성을 가지지 않는 것을 권장합니다.
