295e699b19b09cf3e7f7b6d0863ade1f409a6db3
Hosung <an3735297@naver.com>
2023-10-15 10:19:19 +0900

    fix : findByIdWithProblemDetail 구현 - querydsl

--- poj/src/main/java/com/poj/repository/problem/ProblemRepository.java
+++ poj/src/main/java/com/poj/repository/problem/ProblemRepository.java
@@ -1,7 +1,6 @@
 package com.poj.repository.problem;
 
 import com.poj.entity.problem.Problem;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,12 +9,9 @@ import java.util.Optional;
 /**
 Problem을 저장하는 JPARepository
  */
-public interface ProblemRepository extends JpaRepository<Problem, Long> {
+public interface ProblemRepository extends JpaRepository<Problem, Long>, ProblemRepositoryQuerydsl{
 
     Optional<Problem> findById(Long id); // problem only
-
-    @EntityGraph(attributePaths = {"problemDetail"})
-    Optional<Problem> findWithProblemDetailById(Long id); // fetch join
 
     // paging
     // 필요한 검색기능
