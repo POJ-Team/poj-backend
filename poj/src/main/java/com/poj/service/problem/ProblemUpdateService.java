1dae04fec7e8ce3f93650a10429a4247258e1a47
haenghaeng <haeng@pusan.ac.kr>
2023-10-30 21:24:10 +0900

    feat:ProblemupdateService
    ID와 request를 받아 문제의 내용을 수정하는 서비스입니다.

---
+++ poj/src/main/java/com/poj/service/problem/ProblemUpdateService.java
@@ -0,0 +1,39 @@
+package com.poj.service.problem;
+
+import com.poj.dto.problem.ProblemDetailRequest;
+import com.poj.dto.problem.ProblemRequest;
+import com.poj.entity.problem.Problem;
+import com.poj.repository.problem.ProblemRepository;
+import com.poj.repository.problem.ProblemRepositoryQuerydsl;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * 문제의 내용을 수정합니다.
+ */
+
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ProblemUpdateService {
+    private final ProblemRepository problemRepository;
+    private final ProblemRepositoryQuerydsl problemRepositoryQuerydsl;
+
+    public void UpdateProblem(Long ID, ProblemRequest problemRequest){
+        if(problemRepository.findById(ID).isPresent()){
+            Problem problem = problemRepository.findById(ID).get();
+            problem.update(problemRequest);
+        }
+    }
+
+    public void UpdateProblemWithDetail(Long ID, ProblemRequest problemRequest, ProblemDetailRequest problemDetailRequest){
+        if(problemRepository.findById(ID).isPresent()){
+            Problem problem = problemRepository.findById(ID).get();
+            problem.updateWithDetail(problemRequest, problemDetailRequest);
+        }
+    }
+
+
+}
