0cad569b993d0214185a34990bdd97db5d1af7b9
Hosung <an3735297@pusan.ac.kr>
2023-11-01 19:18:35 +0900

    feat : repository / Member 를 Authority 기반으로 조회하기
    Member 는 ROLE_ADMIN 인 경우 ROLE_UNVERIFIED 와 ROLE_USER 의 권한도 가지고 있습니다. 하지만 답변할 경우 ROLE_ADMIN 만 답변해야하기에 이를 위한 util 클래스를 만들고 페이지네이션을 구현하였습니다.

--- poj/src/main/java/com/poj/repository/member/MemberRepositoryQuerydslImpl.java
+++ poj/src/main/java/com/poj/repository/member/MemberRepositoryQuerydslImpl.java
@@ -1,10 +1,16 @@
 package com.poj.repository.member;
 
+import com.poj.dto.member.BasicMemberDto;
+import com.poj.entity.member.Authority;
+import com.poj.entity.member.EAuthority;
 import com.poj.entity.member.Member;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.*;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.poj.entity.member.QMember.*;
@@ -35,5 +41,30 @@ public class MemberRepositoryQuerydslImpl implements MemberRepositoryQuerydsl{
                         .where(member.id.eq(id))
                         .fetchFirst()
         );
+    }
+
+    @Override
+    public Page<BasicMemberDto> findAllByAuthority(EAuthority authority, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "createdAt"));
+        List<Member> content = query
+                .selectFrom(member)
+                .leftJoin(member.authorities).fetchJoin()
+                .where(authorityFilter(authority))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .orderBy(member.createdAt.asc())
+                .fetch();
+        List<BasicMemberDto> dtoContent =
+                content.stream().map(BasicMemberDto::new).toList();
+        int totalCount = query.selectFrom(member).where(authorityFilter(authority)).fetch().size();
+
+        return new PageImpl<>(dtoContent, pageable, totalCount);
+    }
+
+    private BooleanBuilder authorityFilter(EAuthority authority) {
+        if (authority == null) {
+            return null;
+        }
+        return new BooleanBuilder(member.authorities.size().eq(authority.getLevel()));
     }
 }
