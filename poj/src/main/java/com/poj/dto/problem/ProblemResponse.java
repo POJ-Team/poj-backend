b551c9cf4d5546dc4a3ac25f8ac8508fc76f2f53
haenghaeng <haeng@pusan.ac.kr>
2023-11-02 15:01:25 +0900

    refactor:ProblemResponse
    ProblemDetail은 ProblemDetailResponse에서 별도로 처리하므로 여기서 제거하였습니다.
    또한 DTO로 바꾸는 method를 추가하였습니다.

--- poj/src/main/java/com/poj/dto/problem/ProblemResponse.java
+++ poj/src/main/java/com/poj/dto/problem/ProblemResponse.java
@@ -2,7 +2,7 @@ package com.poj.dto.problem;
 
 import com.poj.entity.problem.EAvailableLanguage;
 import com.poj.entity.problem.EProblemDifficulty;
-import com.poj.entity.problem.ProblemDetail;
+import com.poj.entity.problem.Problem;
 import lombok.Data;
 
 import java.util.Set;
@@ -13,12 +13,14 @@ public class ProblemResponse {
     private String title; // 제목
     private EProblemDifficulty difficulty; // 난이도
     private Set<EAvailableLanguage> availableLanguage; // 문제에서 사용 가능한 언어 집합
-    private ProblemDetail problemDetail; // 문제의 상세 정보
 
-    public ProblemResponse(String title, EProblemDifficulty difficulty, Set<EAvailableLanguage> availableLanguage, ProblemDetail problemDetail) {
+    public ProblemResponse(String title, EProblemDifficulty difficulty, Set<EAvailableLanguage> availableLanguage) {
         this.title = title;
         this.difficulty = difficulty;
         this.availableLanguage = availableLanguage;
-        this.problemDetail = problemDetail; // problemDetail에 대한 response도 필요할까요?
+    }
+    
+    public static ProblemResponse toDTO(Problem problem){
+        return new ProblemResponse(problem.getTitle(), problem.getDifficulty(), problem.getAvailableLanguage());
     }
 }
