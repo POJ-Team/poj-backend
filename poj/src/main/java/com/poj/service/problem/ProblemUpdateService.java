09e49e1837b8e068bdba3e8667da9b004f78d3cb
haenghaeng <haeng@pusan.ac.kr>
2023-11-02 14:56:20 +0900

    refactor:ProblemUpdateService
    ProblemRequest, ProblemDetailRequest를 ProblemCreateRequest로 합쳤으므로 이에 맞춰 변경합니다

--- poj/src/main/java/com/poj/service/problem/ProblemUpdateService.java
+++ poj/src/main/java/com/poj/service/problem/ProblemUpdateService.java
@@ -1,10 +1,8 @@
 package com.poj.service.problem;
 
-import com.poj.dto.problem.ProblemDetailRequest;
-import com.poj.dto.problem.ProblemRequest;
+import com.poj.dto.problem.ProblemCreateRequest;
 import com.poj.entity.problem.Problem;
 import com.poj.repository.problem.ProblemRepository;
-import com.poj.repository.problem.ProblemRepositoryQuerydsl;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,23 +17,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProblemUpdateService {
     private final ProblemRepository problemRepository;
-    private final ProblemRepositoryQuerydsl problemRepositoryQuerydsl;
 
-    public void UpdateProblem(Long ID, ProblemRequest problemRequest){
+    public void UpdateProblem(Long ID, ProblemCreateRequest problemCreateRequest){
         if(problemRepository.findById(ID).isPresent()){
             Problem problem = problemRepository.findById(ID).get();
-            problem.update(problemRequest);
+            problem.update(problemCreateRequest);
             problemRepository.save(problem);
         }
     }
-
-    public void UpdateProblemWithDetail(Long ID, ProblemRequest problemRequest, ProblemDetailRequest problemDetailRequest){
-        if(problemRepository.findById(ID).isPresent()){
-            Problem problem = problemRepository.findById(ID).get();
-            problem.updateWithDetail(problemRequest, problemDetailRequest);
-            problemRepository.save(problem);
-        }
-    }
-
-
 }
