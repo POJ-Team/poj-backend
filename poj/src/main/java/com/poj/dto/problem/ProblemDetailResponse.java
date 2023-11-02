322e47dcb10cbf0c34e0b2f8c7b15be1657d7345
haenghaeng <haeng@pusan.ac.kr>
2023-11-02 15:02:44 +0900

    feat:ProblemDetailResponse
    문제의 Detail 정보만 전달하는 Response를 만들었습니다.

--- poj/src/main/java/com/poj/dto/problem/ProblemDetailResponse.java
+++ poj/src/main/java/com/poj/dto/problem/ProblemDetailResponse.java
@@ -1,11 +1,8 @@
 package com.poj.dto.problem;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 
-@Data
-public class ProblemDetailRequest {
-
+public class ProblemDetailResponse {
     @NotBlank
     private String info; // 문제 설명
 
@@ -14,4 +11,12 @@ public class ProblemDetailRequest {
 
     private Long timeLimit; // 시간 제한(default = Long.MAX_VALUE)
     private Long memoryLimit; // 메모리 제한. 단위 = mb(default = 256mb)
+
+    public ProblemDetailResponse(String info, String inputExample, String outputExample, Long timeLimit, Long memoryLimit) {
+        this.info = info;
+        this.inputExample = inputExample;
+        this.outputExample = outputExample;
+        this.timeLimit = timeLimit;
+        this.memoryLimit = memoryLimit;
+    }
 }
