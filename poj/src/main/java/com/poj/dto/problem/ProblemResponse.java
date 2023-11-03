83fd79f6ab5ea0e861b582e0aa7b6120dd9342b6
haenghaeng <haeng@pusan.ac.kr>
2023-11-03 12:20:50 +0900

    refactor : ProblemResponse
    문제 ID를 추가하고 DTO를 만드는 method를 Builder 패턴으로 변경하였습니다.

--- poj/src/main/java/com/poj/dto/problem/ProblemResponse.java
+++ poj/src/main/java/com/poj/dto/problem/ProblemResponse.java
@@ -3,24 +3,27 @@ package com.poj.dto.problem;
 import com.poj.entity.problem.EAvailableLanguage;
 import com.poj.entity.problem.EProblemDifficulty;
 import com.poj.entity.problem.Problem;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.Set;
 
 @Data
+@Builder
 public class ProblemResponse {
+
+    private Long id; // 문제 ID
 
     private String title; // 제목
     private EProblemDifficulty difficulty; // 난이도
     private Set<EAvailableLanguage> availableLanguage; // 문제에서 사용 가능한 언어 집합
 
-    public ProblemResponse(String title, EProblemDifficulty difficulty, Set<EAvailableLanguage> availableLanguage) {
-        this.title = title;
-        this.difficulty = difficulty;
-        this.availableLanguage = availableLanguage;
-    }
-    
     public static ProblemResponse toDTO(Problem problem){
-        return new ProblemResponse(problem.getTitle(), problem.getDifficulty(), problem.getAvailableLanguage());
+        return ProblemResponse.builder()
+                .id(problem.getId())
+                .title(problem.getTitle())
+                .difficulty(problem.getDifficulty())
+                .availableLanguage(problem.getAvailableLanguage())
+                .build();
     }
 }
