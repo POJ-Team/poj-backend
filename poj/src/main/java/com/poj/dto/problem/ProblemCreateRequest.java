a3715c2a6bc45cd8d75ad3b3c32ba9afff18dfa6
haenghaeng <haeng@pusan.ac.kr>
2023-11-03 11:17:55 +0900

    fix:ProblemCreateRequest
    Detail에 대한 정보가 누락되어 추가합니다.

--- poj/src/main/java/com/poj/dto/problem/ProblemCreateRequest.java
+++ poj/src/main/java/com/poj/dto/problem/ProblemCreateRequest.java
@@ -23,4 +23,13 @@ public class ProblemCreateRequest {
 
     @Enumerated(EnumType.STRING)
     private Set<EAvailableLanguage> availableLanguage; // 문제에서 사용 가능한 언어 집합
+
+    @NotBlank
+    private String info; // 문제 설명
+
+    private String inputExample; // input 예시
+    private String outputExample; // output 예시
+
+    private Long timeLimit; // 시간 제한(default = Long.MAX_VALUE)
+    private Long memoryLimit; // 메모리 제한. 단위 = mb(default = 256mb)
 }
